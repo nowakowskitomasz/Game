f23b2d68622feccf157e51629a6e6cd3d87a2701
Tomasz Nowakowski <nowakowski.tomasz88@gmail.com>
2018-10-04 18:03:30 +0200

    Try build object and buttons loop

--- js/index.js
+++ js/index.js
@@ -2,85 +2,99 @@
 
 (function(){
   
-  var output = document.getElementById('greeter-output');
-  var paper = document.getElementById('greeter-button1');
-  var rock = document.getElementById('greeter-button2');
-  var scissors = document.getElementById('greeter-button3');
-  var result = document.getElementById('result');
-  var newGame = document.getElementById('new-game');
-  var gameInfo = document.getElementById('game-info');
-  var playerChoice;
-  var computerChoice;
-  var possibleChoices = ['paper', 'rock', 'scissors'];
-  var playerScore = 0;
-  var computerScore = 0;
-  var pointsToWon;
-  
-  var playerMove = function(choice){
-    return playerChoice = choice;
+  var params = {
+    var output = document.getElementById('greeter-output'),
+    var paper = document.getElementById('greeter-button1'),
+    var rock = document.getElementById('greeter-button2'),
+    var scissors = document.getElementById('greeter-button3'),
+    var result = document.getElementById('result'),
+    var newGame = document.getElementById('new-game'),
+    var gameInfo = document.getElementById('game-info'),
+    var playerChoice,
+    var computerChoice,
+    var possibleChoices = ['paper', 'rock', 'scissors'],
+    var playerScore = 0,
+    var computerScore = 0,
+    var pointsToWon,
   }
   
+  var playerMove = function(choice){
+    return params.playerChoice = choice;
+  }
+  
+  '''
+  var allButtons = function(){
+    var buttons = document.querySellectorAll('.player-move');
+    for(var i = 0; i < buttons.length; i++){
+      var paper = greeter-button1.getAttribute('data-move');
+      var rock = greeter-button2.getAttribute('data-move');
+      var scissors = greeter-button3.getAttribute('data-move');
+      playerMove(data-move);
+    }
+  }
+  '''
+
   var computerMove = function(){
     var index = Math.floor(Math.random() * 3);
-    return computerChoice = possibleChoices[index];
+    return params.computerChoice = params.possibleChoices[index];
   }
       
   var compareChoices = function(){
-    if (playerChoice === computerChoice){
-      output.innerHTML = 'You choose ' + playerChoice + ' , computer choose ' + computerChoice + ' . REMIS !!!';
+    if (params.playerChoice === params.computerChoice){
+      params.output.innerHTML = 'You choose ' + params.playerChoice + ' , computer choose ' + params.computerChoice + ' . REMIS !!!';
     }
-    if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-      output.innerHTML = 'You choose ' + playerChoice + ' , computer choose ' + computerChoice + ' . You won !!!';
+    if ((params.playerChoice === 'paper' && params.computerChoice === 'rock') || (params.playerChoice === 'rock' && params.computerChoice === 'scissors') || (params.playerChoice === 'scissors' && params.computerChoice === 'paper')){
+      params.output.innerHTML = 'You choose ' + params.playerChoice + ' , computer choose ' + params.computerChoice + ' . You won !!!';
     }
-    if ((playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'scissors' && computerChoice === 'rock')){
-      'You choose ' + playerChoice + ' , computer choose ' + computerChoice + ' . You loose !!!';
+    if ((params.playerChoice === 'paper' && params.computerChoice === 'scissors') || (params.playerChoice === 'rock' && params.computerChoice === 'paper') || (params.playerChoice === 'scissors' && params.computerChoice === 'rock')){
+      'You choose ' + params.playerChoice + ' , computer choose ' + params.computerChoice + ' . You loose !!!';
     }
   }
     
   var score = function(){
-    if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')){
-      playerScore++;
-      return result.innerHTML = 'SCORE: ' + playerScore + ' to ' + computerScore;
+    if ((params.playerChoice === 'paper' && params.computerChoice === 'rock') || (params.playerChoice === 'rock' && params.computerChoice === 'scissors') || (params.playerChoice === 'scissors' && params.computerChoice === 'paper')){
+      params.playerScore++;
+      return params.result.innerHTML = 'SCORE: ' + params.playerScore + ' to ' + params.computerScore;
     }
-    if ((playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'scissors' && computerChoice === 'rock')){
-      computerScore++;
-      return result.innerHTML = 'SCORE: ' + playerScore + ' to ' + computerScore;
+    if ((params.playerChoice === 'paper' && params.computerChoice === 'scissors') || (params.playerChoice === 'rock' && params.computerChoice === 'paper') || (params.playerChoice === 'scissors' && params.computerChoice === 'rock')){
+      params.computerScore++;
+      return params.result.innerHTML = 'SCORE: ' + params.playerScore + ' to ' + params.computerScore;
     }
   }
   
   var gameOver = function(){
-    if (playerScore === pointsToWon){
+    if (params.playerScore === params.pointsToWon){
       toggleButtons();
-      return gameInfo.innerHTML = 'YOU ARE WINNER !!! SCORE: ' + playerScore + ' to ' + computerScore + '<br>' + 'Click NEW GAME';
+      return params.gameInfo.innerHTML = 'YOU ARE WINNER !!! SCORE: ' + params.playerScore + ' to ' + params.computerScore + '<br>' + 'Click NEW GAME';
     }
-    if (computerScore === pointsToWon){
+    if (params.computerScore === params.pointsToWon){
       toggleButtons();
-      return gameInfo.innerHTML = 'YOU ARE LOOSER !!! SCORE: ' + playerScore + ' to ' + computerScore + '<br>' + 'Click NEW GAME';
+      return params.gameInfo.innerHTML = 'YOU ARE LOOSER !!! SCORE: ' + params.playerScore + ' to ' + params.computerScore + '<br>' + 'Click NEW GAME';
     }
-    return gameInfo.innerHTML = 'This game has a ' + pointsToWon + ' rounds';
+    return params.gameInfo.innerHTML = 'This game has a ' + params.pointsToWon + ' rounds';
   }
   
   var toggleButtons = function(){
-    paper.disabled = !paper.disabled;
-    rock.disabled = !rock.disabled;
-    scissors.disabled = !scissors.disabled;
+    params.paper.disabled = !params.paper.disabled;
+    params.rock.disabled = !params.rock.disabled;
+    params.scissors.disabled = !params.scissors.disabled;
   } 
   
   var resetPoints = function(){
-    pointsToWon = null;
-    playerScore = 0;
-    computerScore = 0;
+    params.pointsToWon = null;
+    params.playerScore = 0;
+    params.computerScore = 0;
   }
   
   var resetText = function(){
-    output.innerHTML = '';
-    result.innerHTML = '';
+    params.output.innerHTML = '';
+    params.result.innerHTML = '';
   }
   
-  newGame.addEventListener('click', function(){
+  params.newGame.addEventListener('click', function(){
     resetPoints();
     toggleButtons();
-    pointsToWon = parseInt(window.prompt('How many rounds do you want to play?'));
+    params.pointsToWon = parseInt(window.prompt('How many rounds do you want to play?'));
     resetText();
     gameOver();
   });
@@ -93,15 +107,15 @@
     gameOver();
   }
   
-  paper.addEventListener('click', function(){
+  params.paper.addEventListener('click', function(){
     shortCut('paper');
   });
   
-  rock.addEventListener('click', function(){
+  params.rock.addEventListener('click', function(){
     shortCut('rock');
   });
   
-  scissors.addEventListener('click', function(){
+  params.scissors.addEventListener('click', function(){
     shortCut('scissors');
   });
   
